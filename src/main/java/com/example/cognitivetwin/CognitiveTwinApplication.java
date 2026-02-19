1e9f3727bb60c8dc8b15f192a3463bd0c84baa53
AaryanCode69 <aaryanupadhyay68@gmail.com>
2026-02-19 23:47:53 +0530

    core: Initialize repo

---
+++ src/main/java/com/example/cognitivetwin/CognitiveTwinApplication.java
@@ -0,0 +1,13 @@
+package com.example.cognitivetwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveTwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitiveTwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cognitivetwin/CognitiveTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cognitivetwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiveTwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
